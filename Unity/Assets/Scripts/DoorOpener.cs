de4c07f5c853ca44d6091e9e1761c58061904d16
Michal <mmichal1526@gmail.com>
2020-01-17 14:12:20 +0100

    Doors added

---
+++ Unity/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!isOpen)
+            {
+                this.transform.GetChild(0).Rotate(0,0,90);
+                isOpen = true;
+            }
+        }
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (isOpen)
+            {
+                this.transform.GetChild(0).Rotate(0, 0, -90);
+                isOpen = false;
+            }
+        }
+    }
+
+
+}
